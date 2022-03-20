aa3836489bd9ea978986d07d5baa1d173d48efc1
Philip Riecks <mail@philipriecks.de>
2022-03-20 17:37:10 +0100

    add sample Markdown preview

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   purge: [
@@ -18,5 +19,13 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+      plugin(function ({addBase, theme}) {
+          addBase({
+              'h1': {fontSize: theme('fontSize.2xl')},
+              'h2': {fontSize: theme('fontSize.xl')},
+              'h3': {fontSize: theme('fontSize.lg')},
+              'ol,ul': {listStyleType: 'revert'}
+          })
+    })
   ]
 };
